fde89439b3b566ea40b9866005106f034f0a3187
MBerthold <MBerthold@NB-FC-215335>
2021-01-27 14:54:04 +0100

    First Version 0

---
+++ src/ReportingRestApiClient.cs
@@ -0,0 +1,179 @@
+﻿namespace Ser.Engine.Rest.Client
+{
+    #region usings
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Text;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+    #endregion
+
+    public class ReportingRestApiClient
+    {
+        #region Properties
+        private HttpClient Client { get; set; }
+        #endregion
+
+        #region Constructor
+        public ReportingRestApiClient(Uri baseAddress, int timeout = 30)
+        {
+            if (timeout < 10)
+                timeout = 10;
+            if (timeout > 300)
+                timeout = 300;
+
+            Client = new HttpClient()
+            {
+                BaseAddress = baseAddress,
+                Timeout = TimeSpan.FromSeconds(timeout)
+            };
+        }
+        #endregion
+
+        private static Guid Generate(Guid? value)
+        {
+            if (value.HasValue)
+                return value.Value;
+            return Guid.NewGuid();
+        }
+
+
+        public Guid Upload(string fullpath, Guid? fileId = null, string filename = null)
+        {
+            try
+            {
+                var uploadId = Generate(fileId);
+
+                var uploadFilename = Path.GetFileName(fullpath);
+                if (!String.IsNullOrEmpty(filename))
+                    uploadFilename = filename;
+
+                var data = File.ReadAllBytes(fullpath);
+                var multiPartContent = new MultipartFormDataContent();
+                var byteArrayContent = new ByteArrayContent(data);
+                byteArrayContent.Headers.Add("Content-Type", "application/octet-stream");
+                multiPartContent.Add(byteArrayContent, "file", uploadFilename);
+
+                var result = Client.PostAsync($"/upload/{uploadId}", multiPartContent).Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var jsonGuid = result.Content.ReadAsStringAsync().Result;
+                    return JsonConvert.DeserializeObject<Guid>(jsonGuid);
+                }
+                throw new Exception(result.ToString());
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("The file upload to rest api failed.", ex);
+            }
+        }
+
+        public byte[] Download(Guid folderId)
+        {
+            try
+            {
+                var result = Client.GetAsync($"/download/{folderId}").Result;
+                if (result.IsSuccessStatusCode)
+                    return result.Content.ReadAsByteArrayAsync().Result;
+                throw new Exception(result.ToString());
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("The file download from rest api failed.", ex);
+            }
+        }
+
+        public bool DeleteFolder(Guid? folderId = null)
+        {
+            try
+            {
+                var requestUri = $"/delete";
+                if (folderId.HasValue)
+                    requestUri = $"/delete/{folderId}";
+                var result = Client.DeleteAsync(requestUri).Result;
+                if (result.IsSuccessStatusCode)
+                    return true;
+                throw new Exception(result.ToString());
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("The folder deletion on rest api failed.", ex);
+            }
+        }
+
+        public Guid RunTask(JObject jsonObject, Guid? taskId = null)
+        {
+            try
+            {
+                var jobTaskId = Generate(taskId);
+                var jsonContent = new StringContent(jsonObject.ToString(), Encoding.UTF8, "application/json");
+                var result = Client.PostAsync($"/task/{jobTaskId}", jsonContent).Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var jsonGuid = result.Content.ReadAsStringAsync().Result;
+                    return JsonConvert.DeserializeObject<Guid>(jsonGuid);
+                }
+                throw new Exception(result.ToString());
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("The task run on rest api failed.", ex);
+            }
+        }
+
+        public bool StopTask(Guid? taskId = null)
+        {
+            try
+            {
+                var requestUri = $"/task";
+                if (taskId.HasValue)
+                    requestUri = $"/task/{taskId}";
+                var result = Client.DeleteAsync(requestUri).Result;
+                if (result.IsSuccessStatusCode)
+                    return true;
+                throw new Exception(result.ToString());
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("The task stopping on rest api failed.", ex);
+            }
+        }
+
+        public JArray GetTasks(Guid? taskId = null)
+        {
+            try
+            {
+                var requestUri = $"/task";
+                if (taskId.HasValue)
+                    requestUri = $"/task/{taskId}";
+                var result = Client.GetAsync(requestUri).Result;
+                if (result.IsSuccessStatusCode)
+                    return JArray.Parse(result.Content.ReadAsStringAsync().Result);
+                throw new Exception(result.ToString());
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("The task get request from rest api failed.", ex);
+            }
+        }
+
+        public string HealthStatus()
+        {
+            try
+            {
+                var result = Client.GetAsync($"/health").Result;
+                if (result.IsSuccessStatusCode)
+                    return result.Content.ReadAsStringAsync().Result;
+                throw new Exception(result.ReasonPhrase);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("The health status request to rest api failed.", ex);
+            }
+        }
+    }
+}
